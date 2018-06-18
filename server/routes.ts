cc4dfa1fbfbb73486948ff0f3d98efc718efea0f
canbemiracles <canbe.miracles@gmail.com>
2018-06-18 17:21:03 +0900

    complete server side

--- server/routes.ts
+++ server/routes.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 
-
 import CatCtrl from './controllers/cat';
+import UserCtrl from './controllers/user';
 
 export default function setRoutes(app) {
   const router = express.Router();
 
   const catCtrl = new CatCtrl();
+  const userCtrl = new UserCtrl();
 
   router.route('/cats').get(catCtrl.getAll);
   router.route('/cats/count').get(catCtrl.count);
@@ -14,6 +15,14 @@ export default function setRoutes(app) {
   router.route('/cat/:id').get(catCtrl.get);
   router.route('/cat/:id').put(catCtrl.update);
   router.route('/cat/:id').delete(catCtrl.delete);
+
+  router.route('/login').post(userCtrl.login);
+  router.route('/users').get(userCtrl.getAll);
+  router.route('/users/count').get(userCtrl.count);
+  router.route('/user').post(userCtrl.insert);
+  router.route('/user/:id').get(userCtrl.get);
+  router.route('/user/:id').put(userCtrl.update);
+  router.route('/user/:id').delete(userCtrl.delete);
 
   app.use('/api', router);
 }
